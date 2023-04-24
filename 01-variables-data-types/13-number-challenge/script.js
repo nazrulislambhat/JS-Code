0871fee3abec7e7d8f1d0195393bba50d5d2fb01
Nazrul Islam <nazrulislambhat@gmail.com>
2023-04-24 15:16:28 +0530

    first commit

---
+++ 01-variables-data-types/13-number-challenge/script.js
@@ -0,0 +1,22 @@
+let x;
+x = Math.floor(Math.random() * 100);
+console.log('Random Number between 1 to 100 is ' + x);
+
+let y;
+y = Math.floor(Math.random() * 50);
+console.log('Random Number between 1 to 50 is ' + y);
+
+let sumOutput = x + y;
+console.log('Sum of output is ' + sumOutput);
+
+let diffOutput = x - y;
+console.log('Difference of output is ' + diffOutput);
+
+let prodOutput = x * y;
+console.log('Prod of output is ' + prodOutput);
+
+let quotOutput = x / y;
+console.log('Quot of output is ' + quotOutput);
+
+let remOutput = x % y;
+console.log('Remander of output is ' + remOutput);
