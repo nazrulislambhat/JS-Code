1843f8a0594523c60abdd8c599fbade13c0e6822
Nazrul Islam <nazrulislambhat@gmail.com>
2023-04-25 10:43:48 +0530

    first commit

--- 01-variables-data-types/15-date-object-methods/script.js
+++ 01-variables-data-types/15-date-object-methods/script.js
@@ -3,3 +3,21 @@ let d = new Date();
 
 x = Intl.DateTimeFormat('default').format(d);
 console.log(x);
+
+x = Intl.DateTimeFormat('default', { month: 'long' }).format(d);
+console.log(x);
+
+x = d.toLocaleString('default', { month: 'short' });
+console.log(x);
+
+
+x = d.toLocaleString('default', {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+});
+console.log(x);
