33b74f8c52aab876d6bd420d921290daf18428fb
Nazrul Islam <nazrulislambhat@gmail.com>
2023-04-26 11:51:59 +0530

    first commit

---
+++ 02-arrays-and-objects/05-object-literals/script.js
@@ -0,0 +1,19 @@
+const person = {
+  name: 'Nazrul Islam',
+  position: 'Software Engineer',
+  age: '31',
+  address: {
+    hno: '127',
+    city: 'srinagar',
+    country: 'india',
+  },
+};
+
+console.log(person);
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.position);
+console.log(person.address.hno);
+console.log(person.address.city);
+console.log(person.address.country);
