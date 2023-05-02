428c44e04acd01e18e1f6e6a7a7465d3406f71b0
Nazrul Islam <nazrulislambhat@gmail.com>
2023-05-02 14:33:47 +0530

    first commit

--- 03-functions-scope/02-params-arguments/script.js
+++ 03-functions-scope/02-params-arguments/script.js
@@ -13,3 +13,22 @@ function sum(...numbers) {
   return total;
 }
 console.log(sum(1, 2, 31));
+
+//objects as parms
+function loginUser(user) {
+  return `the user ${user.name} with the id of ${user.id} is logged in`;
+}
+const user = {
+  id: 17,
+  name: 'Nazrul Islam',
+};
+
+console.log(loginUser(user));
+
+//arrays as params
+function getRandom(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  const item = arr[randomIndex];
+  console.log(item);
+}
+getRandom([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
