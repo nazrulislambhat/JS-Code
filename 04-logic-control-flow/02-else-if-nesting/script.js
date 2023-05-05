a1a0f9e75b57b0627d0641baa2a55587972d952e
Nazrul Islam <nazrulislambhat@gmail.com>
2023-05-05 13:14:20 +0530

    Just commits

--- 04-logic-control-flow/02-else-if-nesting/script.js
+++ 04-logic-control-flow/02-else-if-nesting/script.js
@@ -1,10 +1,24 @@
-const d = new Date(10, 30, 23, 13, 0, 0);
+const d = new Date(10, 30, 23, 21, 0, 0);
 const hour = d.getHours();
+// if (hour < 12) {
+//   console.log('Good Morning');
+// } else if (hour < 18) {
+//   console.log('Good Afternoon');
+// } else {
+//   console.log('Good Evening');
+// }
+//Nested if
+
 if (hour < 12) {
   console.log('Good Morning');
+  if (hour === 6) {
+    console.log('Wakey Wakey');
+  }
 } else if (hour < 18) {
   console.log('Good Afternoon');
 } else {
   console.log('Good Evening');
+  if (hour >= 20) {
+    console.log('Sleepy Sleepy');
+  }
 }
-//Nested if
