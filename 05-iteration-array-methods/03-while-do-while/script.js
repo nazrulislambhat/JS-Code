6068abcde62f19c75dab6a5d9cda7a530f2d6d37
Nazrul Islam <nazrulislambhat@gmail.com>
2023-05-11 12:06:39 +0530

    Just

--- 05-iteration-array-methods/03-while-do-while/script.js
+++ 05-iteration-array-methods/03-while-do-while/script.js
@@ -1,5 +1,10 @@
 let i = 0;
-while (i <= 20) {
-  console.log(i);
+// while (i <= 20) {
+//   console.log(i);
+//   i++;
+// }
+const arr = [20, 30, 40, 50, 60];
+while (i <= arr.length) {
+  console.log(arr[i]);
   i++;
 }
