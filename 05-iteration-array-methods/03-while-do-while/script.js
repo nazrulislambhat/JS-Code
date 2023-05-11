974da406c188f378641ad2a47f3aefb07909caf8
Nazrul Islam <nazrulislambhat@gmail.com>
2023-05-11 12:15:04 +0530

    Just

--- 05-iteration-array-methods/03-while-do-while/script.js
+++ 05-iteration-array-methods/03-while-do-while/script.js
@@ -3,8 +3,21 @@ let i = 0;
 //   console.log(i);
 //   i++;
 // }
+
 const arr = [20, 30, 40, 50, 60];
 while (i <= arr.length) {
   console.log(arr[i]);
   i++;
+}
+
+//nest while loops
+let j = 0;
+while (j <= 10) {
+  console.log('Table of ' + j);
+  let k = 1;
+  while (k <= 10) {
+    console.log(`${j} * ${k} = ${j * k}`);
+    k++;
+  }
+  j++;
 }
