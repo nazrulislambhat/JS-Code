2efedf70d72a8d2cae2c85fe3812345baff15614
Nazrul Islam <nazrulislambhat@gmail.com>
2023-05-12 09:33:35 +0530

    yeah

--- 05-iteration-array-methods/07-forEach/script.js
+++ 05-iteration-array-methods/07-forEach/script.js
@@ -1,6 +1,8 @@
 //High order array methods
-const socials = ['instagram', 'facebook', 'youtube', 'twitter'];
+const socials = ['Instagram', 'Facebook', 'Youtube', 'Twitter'];
 
 socials.forEach((social) => {
-  console.log(social);
+  console.log(social); //will run 4 times in this case becasue we have 4 items in the arrayb for each element it will run once
+  console.log(social[0]); //will run everytime time for each element & will display 0th element of all 4 letters
+  console.log(social[social.length - 1]); //will run everytime time for each element & will display last element of all 4 items in the array
 });
