52043e207f1130fd8d9b35402162733a295d794a
Nazrul Islam <nazrulislambhat@gmail.com>
2023-05-12 10:22:14 +0530

    yeah

--- 05-iteration-array-methods/08-filter/script.js
+++ 05-iteration-array-methods/08-filter/script.js
@@ -3,3 +3,16 @@ const evenNumbers = number.filter(function (number) {
   return number % 2 === 0;
 });
 console.log(evenNumbers);
+
+//short version
+const oddNumbers = number.filter((number) => number % 2 === 1);
+console.log(oddNumbers);
+
+//same with forEach will get the same result but a bit of code
+let even = [];
+number.forEach((number) => {
+  if (number % 2 === 0) {
+    even.push(number);
+  }
+});
+console.log(even);
