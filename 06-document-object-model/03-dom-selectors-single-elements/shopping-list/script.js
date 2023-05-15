34273097b36f85552e36af20ea3d3ebfdb51694b
Nazrul Islam <nazrulislambhat@gmail.com>
2023-05-15 12:15:40 +0530

    DOM

--- 06-document-object-model/03-dom-selectors-single-elements/shopping-list/script.js
+++ 06-document-object-model/03-dom-selectors-single-elements/shopping-list/script.js
@@ -9,3 +9,16 @@ console.log(document.getElementById('app-title').getAttribute('id'));
 
 //Set Attributes
 console.log((document.getElementById('app-title').title = 'Hello World'));
+
+//GET CHANGE CONTENT'
+const title = document.getElementById('app-title');
+console.log(title.textContent);
+title.textContent = 'Hello World';
+title.innerText = 'Hello Worldd';
+
+title.innerHTML = '<strong>Hello World</strong>';
+title.style.color = 'Red';
+title.style.backgroundColor = 'pink';
+
+//document.querySelector
+console.log(document.querySelector('h1')); //selects first h1 in the page
