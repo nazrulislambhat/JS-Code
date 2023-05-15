a48db535b71168263e8a3208f22a1be07c032f61
Nazrul Islam <nazrulislambhat@gmail.com>
2023-05-15 17:15:13 +0530

    DOM

--- 06-document-object-model/06-traversing-the-dom-nodes/script.js
+++ 06-document-object-model/06-traversing-the-dom-nodes/script.js
@@ -2,6 +2,20 @@
 let output;
 
 const parent = document.querySelector('.parent');
-output.parent.children;
 
+output = parent.children;
+
+output = parent.firstElementChild;
+output = parent.lastElementChild;
+
+//Get Parent Elements from CHild
+
+const child = document.querySelector('.child');
+output = child.parentElement; //outputting parent by using child
+
+//Select Siblling elements
+
+const child2 = document.querySelector('.child:nth-child(2)');
+output = child2.previousElementSibling; //previous child1
+output = child2.nextElementSibling; //next child3
 console.log(output);
