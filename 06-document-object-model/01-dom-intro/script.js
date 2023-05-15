53f42602c5ca344d5905b2aa76cc67fb23973572
Nazrul Islam <nazrulislambhat@gmail.com>
2023-05-15 11:41:47 +0530

    DOM

--- 06-document-object-model/01-dom-intro/script.js
+++ 06-document-object-model/01-dom-intro/script.js
@@ -8,3 +8,4 @@ console.log(document.body.innerText);
 
 // document.body.innerHTML = 'Hello World';
 document.write('Stupid'); // dont see this used that much because their is no target
+console.log(document.getElementById('main'));
