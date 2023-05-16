07c544d995b292c041a009334ebb5d743a911c4b
Nazrul Islam <nazrulislambhat@gmail.com>
2023-05-16 10:48:16 +0530

    DOM

--- 06-document-object-model/07-create-elements/shopping-list/script.js
+++ 06-document-object-model/07-create-elements/shopping-list/script.js
@@ -12,3 +12,36 @@ document.querySelector('ul').appendChild(div);
 document.body.appendChild(div);
 
 console.log(div);
+
+//creating element & adding to DOM
+//Quick & Dirty
+function createListItem(item) {
+  const li = document.createElement('li');
+
+  li.innerHTML = `${item}
+    <button class="remove-item btn-link text-red">
+            <i class="fa-solid fa-xmark"></i>
+          </button>`;
+
+  document.querySelector('.items').appendChild(li);
+}
+
+//Clean
+
+function createNewItem(item) {
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(item));
+
+  const button = document.createElement('button');
+  button.className = 'remove-item btn-link text-red';
+
+  const icon = document.createElement('i');
+  icon.className = 'fa-solid fa-xmark';
+
+  button.appendChild(icon);
+  li.appendChild(button);
+  console.log(li.innerHTML);
+  document.querySelector('.items').appendChild(li);
+}
+createListItem('Eggs');
+createNewItem('Cheese');
