4d699ff797f286847fd2f64dfd920ff8f4c97020
Nazrul Islam <nazrulislambhat@gmail.com>
2023-05-17 11:07:21 +0530

    yeah

--- 07-events/02-mouse-events/shopping-list/script.js
+++ 07-events/02-mouse-events/shopping-list/script.js
@@ -1,1 +1,19 @@
+const logo = document.querySelector('img');
 
+const onClick = () => console.log('click event');
+
+// const onDoubleClick = () => console.log('double click event');
+
+const onDoubleClick = () => {
+  if (document.body.style.backgroundColor !== 'aquamarine') {
+    document.body.style.backgroundColor = 'aquamarine';
+    document.body.style.color = 'red';
+  } else {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+  }
+};
+
+//Event Listeners
+logo.addEventListener('click', onClick);
+logo.addEventListener('dblclick', onDoubleClick);
