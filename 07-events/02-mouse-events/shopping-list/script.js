a89c0c98b9b0ee0d9efa67194e156762abbbe85c
Nazrul Islam <nazrulislambhat@gmail.com>
2023-05-17 11:36:52 +0530

    yeah

--- 07-events/02-mouse-events/shopping-list/script.js
+++ 07-events/02-mouse-events/shopping-list/script.js
@@ -14,6 +14,30 @@ const onDoubleClick = () => {
   }
 };
 
+const onRightClick = () => console.log('right click event');
+
+const onMouseDownClick = () => console.log('mouse down event');
+const onMouseUpClick = () => console.log('mouse up event');
+
+const onWheel = () => console.log('wheel');
+
+const onMouseOut = () => console.log('mouse out');
+const onMouseOver = () => console.log('mouse over');
+
+const onDragStart = () => console.log('dragging started Start'); //only once on whick when dragging starts
+const onDragEnd = () => console.log('dragging started end'); //only once on whick when dragging lets go
+
+const onDrag = () => console.log('dragging'); //aslong as user is draggin or holding
+
 //Event Listeners
 logo.addEventListener('click', onClick);
 logo.addEventListener('dblclick', onDoubleClick);
+logo.addEventListener('contextmenu', onRightClick);
+logo.addEventListener('mousedown', onMouseDownClick);
+logo.addEventListener('mouseup', onMouseUpClick);
+logo.addEventListener('wheel', onWheel);
+logo.addEventListener('mouseover', onMouseOver);
+logo.addEventListener('mouseout', onMouseOut);
+logo.addEventListener('dragstart', onDragStart);
+logo.addEventListener('dragend', onDragEnd);
+logo.addEventListener('drag', onDrag);
