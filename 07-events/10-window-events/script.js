55114a09f99fef8b6d155e2716d2c6648f6916cd
Nazrul Islam <nazrulislambhat@gmail.com>
2023-05-19 16:48:28 +0530

    Events

--- 07-events/10-window-events/script.js
+++ 07-events/10-window-events/script.js
@@ -1,1 +1,22 @@
+// document.querySelector('h1').textContent = 'Hello World';
+// //will output null because we're loading js on page top before the DOM is loaded
+
+// window.onload = function () {
+//   document.querySelector('h1').textContent = 'Hello World';
+// };
+// //this will work like a kinda hack
+
+// window.addEventListener('load', () => {
+//   document.querySelector('h1').textContent = 'Hello World';
+// }); //will wait for the entire page to load
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   document.querySelector('h1').textContent = 'Hello World';
+// });//as soon as the DOM is parsed this will work
+
+// window.addEventListener('resize', () => {
+//   document.querySelector(
+//     'h1'
+//   ).innerText = `${window.innerWidth} x ${window.innerHeight}`;
+// }); 
 
