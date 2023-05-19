79bb170aa1f1ab7cec8edc15c127e4595d3298ba
Nazrul Islam <nazrulislambhat@gmail.com>
2023-05-19 15:41:26 +0530

    Events

---
+++ 07-events/09-event-delegation/shopping-list/script.js
@@ -0,0 +1,22 @@
+const listItems = document.querySelectorAll('li');
+
+const list = document.querySelector('ul');
+
+// listItems.forEach((item) => {
+//   item.addEventListener('click', (e) => {
+//     e.target.remove();
+//   });
+// });
+
+//USE EVENT DELEGATION
+list.addEventListener('click', (e) => {
+  if (e.target.tagName === 'LI') {
+    e.target.remove();
+  }
+});
+
+list.addEventListener('mouseover', (e) => {
+  if (e.target.tagName === 'LI') {
+    e.target.style.color = 'green';
+  }
+});
