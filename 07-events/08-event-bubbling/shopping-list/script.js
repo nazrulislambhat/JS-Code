250a43a8a9902acfc7dfbf3b53567139a7dae315
Nazrul Islam <nazrulislambhat@gmail.com>
2023-05-19 11:54:03 +0530

    Events

--- 07-events/08-event-bubbling/shopping-list/script.js
+++ 07-events/08-event-bubbling/shopping-list/script.js
@@ -1,3 +1,12 @@
+/*
+With bubbling the event is first captured and handeled by the innnermost element and then propagated to the outer elements eg
+
+Button<<Div<<Body<<HTML<<Document
+
+
+If we have click event on all these components the BUtton will be firred first & all other kinda bubbled on that &  the document witll be fired last
+*/
+
 const button = document.querySelector('button');
 const div = document.querySelector('form div:nth-child(2)');
 
@@ -6,5 +15,5 @@ button.addEventListener('click', () => {
 });
 
 div.addEventListener('click', () => {
-  alert('clicked');
+  alert('div clicked');
 });
