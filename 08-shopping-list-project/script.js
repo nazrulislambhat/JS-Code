9d5178bfa0c93d44c747f1e5b79dfbff89d83fc9
Nazrul Islam <nazrulislambhat@gmail.com>
2023-05-26 10:41:35 +0530

    Shopping List Project

--- 08-shopping-list-project/script.js
+++ 08-shopping-list-project/script.js
@@ -2,6 +2,7 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 function addItem(e) {
   e.preventDefault();
@@ -21,8 +22,9 @@ function addItem(e) {
   const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
 
+  //add li to DOM
   itemList.appendChild(li);
-
+  resetUI();
   itemInput.value = '';
 }
 
@@ -42,7 +44,10 @@ function createIcon(classes) {
 
 function removeItem(e) {
   if (e.target.parentElement.classList.contains('remove-item')) {
-    e.target.parentElement.parentElement.remove();
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+      resetUI();
+    }
   }
 }
 
@@ -50,12 +55,23 @@ function clearItems() {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
   }
+  resetUI();
 }
 
-function checkUI() {
-    
+function resetUI() {
+  const items = itemList.querySelectorAll('li');
+  if (items.length === 0) {
+    clearBtn.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearBtn.style.display = 'block';
+    itemFilter.style.display = 'block';
+  }
 }
+
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems);
+
+resetUI();
