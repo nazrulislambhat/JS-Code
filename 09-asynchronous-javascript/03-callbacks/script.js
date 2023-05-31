e2d1dea714b9f87ba1a73cd1e39567bf32a83431
Nazrul Islam <nazrulislambhat@gmail.com>
2023-05-31 17:50:43 +0530

    Async JS

--- 09-asynchronous-javascript/03-callbacks/script.js
+++ 09-asynchronous-javascript/03-callbacks/script.js
@@ -4,3 +4,26 @@ function toggle(e) {
 }
 
 document.querySelector('button').addEventListener('click', toggle);
+
+const posts = [
+  { title: 'Post one', body: 'This is post one body' },
+  { title: 'Post two', body: 'This is post two body' },
+];
+function createPost(post, callback) {
+  setTimeout(() => {
+    posts.push(post);
+    callback();
+  }, 2000);
+}
+
+function getPosts() {
+  setTimeout(() => {
+    posts.forEach(function (post) {
+      const div = document.createElement('div');
+      div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
+      document.querySelector('#posts').appendChild(div);
+    });
+  }, 1000);
+}
+
+createPost({ title: 'Post three', body: 'This is post three body' }, getPosts);
