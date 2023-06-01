26187c1b58bad2ec751f4991bab47a08721b2e31
Nazrul Islam <nazrulislambhat@gmail.com>
2023-06-01 10:06:46 +0530

    Hello

--- 09-asynchronous-javascript/04-ajax-xhr/script.js
+++ 09-asynchronous-javascript/04-ajax-xhr/script.js
@@ -1,6 +1,8 @@
 const xhr = new XMLHttpRequest();
 
-xhr.open('GET', './github.json');
+//xhr.open('GET', './github.json');
+
+xhr.open('GET', 'https://api.github.com/users/nazrulislambhat/repos');
 
 /* 
 readtState has 5 possible states
@@ -24,14 +26,31 @@ readtState has 5 possible states
 
 // xhr.send();
 
+
+//fetching user info
+
+// xhr.onreadystatechange = function () {
+//   if (this.readyState === 4 && this.status === 200) {
+//     console.log(JSON.parse(this.responseText));
+//     const data = JSON.parse(this.responseText);
+//     const dataArray = Object.entries(data);
+//     dataArray.forEach(([key, value]) => {
+//       const li = document.createElement('li');
+//       li.innerHTML = `<strong>${key}</strong> - ${value}`;
+//       document.querySelector('#results').appendChild(li);
+//     });
+//   }
+// };
+
+//fetching repos
+
 xhr.onreadystatechange = function () {
   if (this.readyState === 4 && this.status === 200) {
-    console.log(JSON.parse(this.responseText));
-    const data = JSON.parse(this.responseText);
-    const dataArray = Object.entries(data);
-    dataArray.forEach(([key, value]) => {
+      const data = JSON.parse(this.responseText);
+      
+    data.forEach((repo) => {
       const li = document.createElement('li');
-      li.innerHTML = `<strong>${key}</strong> - ${value}`;
+      li.innerHTML = `<strong>${repo.name}</strong> - ${repo.description}`;
       document.querySelector('#results').appendChild(li);
     });
   }
