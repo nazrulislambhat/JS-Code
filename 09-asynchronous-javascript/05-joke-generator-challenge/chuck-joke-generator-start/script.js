22be2a74e24170ffd6a2d23ce67edaa8d6b9651f
Nazrul Islam <nazrulislambhat@gmail.com>
2023-06-01 11:22:55 +0530

    Hello

---
+++ 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator-start/script.js
@@ -0,0 +1,20 @@
+const jokeBtn = document.getElementById('joke-btn');
+const jokeEl = document.getElementById('joke');
+
+const generateJoke = () => {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      jokeEl.innerHTML = JSON.parse(this.responseText).value;
+    } else {
+      jokeEl.innerHTML = 'Something went wrong';
+    }
+  };
+  xhr.send();
+};
+
+jokeBtn.addEventListener('click', generateJoke);
+document.addEventListener('DOMContentLoaded', generateJoke);
