3c482e21f5c02fa9f15a54921c2ad2f2d9c8b6d6
Nazrul Islam <nazrulislambhat@gmail.com>
2023-06-02 13:16:14 +0530

    Hello

--- 09-asynchronous-javascript/11-promise-all/script.js
+++ 09-asynchronous-javascript/11-promise-all/script.js
@@ -20,16 +20,14 @@ function getData(endpoint) {
   });
 }
 
-getData('./movies.json')
-  .then((movies) => {
-    console.log(movies);
-    return getData('./actors.json');
+const moviesPromise = getData('./movies.json');
+const actorsPromise = getData('./actors.json');
+const directorsPromise = getData('./directors.json');
+
+Promise.all([moviesPromise, actorsPromise, directorsPromise])
+  .then((data) => {
+    console.log(data);
   })
-  .then((actors) => {
-    console.log(actors);
-    return getData('./directors.json');
-  })
-  .then((directors) => {
-    console.log(directors);
-  })
-  .catch((error) => console.log(error));
+  .catch((error) => {
+    console.log(error);
+  });
