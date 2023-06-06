9655eea4e54f6900f9a022a2c781c4f9793fbc34
Nazrul Islam <nazrulislambhat@gmail.com>
2023-06-06 20:16:43 +0530

    fetch async

--- 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
+++ 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -1,4 +1,3 @@
-const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 /*
 The reason why the getTodos function is returning a Promise is because the fetch function itself returns a Promise. The fetch function is an asynchronous operation that sends a network request to the specified URL and returns a Promise that resolves to the Response object representing the response to that request.
 
@@ -6,30 +5,84 @@ In the code you provided, the fetch function is used to send a GET request to th
 
 Finally, in the second .then() callback, the data parameter represents the parsed JSON data obtained from the response. In your code, it logs the data to the console. If you want to work with the data outside of the getTodos function, you can return the Promise chain using the return keyword, allowing the caller of getTodos to handle the resolved Promise and access the data.
 */
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
 const getTodos = () => {
   fetch(apiUrl + '?_limit=10')
     .then((res) => res.json())
     .then((data) => {
-      data.forEach((todo) => addTodoToDom(todo));
+      data.forEach((todo) => addTodoToDOM(todo));
     });
 };
 
-function addTodoToDom(todo) {
+const addTodoToDOM = (todo) => {
   const div = document.createElement('div');
+  div.classList.add('todo');
   div.appendChild(document.createTextNode(todo.title));
-  div.setAttribute('data-id', todo.id); //custom attribute
+  div.setAttribute('data-id', todo.id);
+
   if (todo.completed) {
     div.classList.add('done');
   }
+
   document.getElementById('todo-list').appendChild(div);
-}
-const createToDo = (e) => {
+};
+
+const createTodo = (e) => {
   e.preventDefault();
-  console.log(e.target.firstElementChild.value);
+
+  const newTodo = {
+    title: e.target.firstElementChild.value,
+    completed: false,
+  };
+
+  fetch(apiUrl, {
+    method: 'POST',
+    body: JSON.stringify(newTodo),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => addTodoToDOM(data));
+};
+
+const toggleCompleted = (e) => {
+  if (e.target.classList.contains('todo')) {
+    e.target.classList.toggle('done');
+
+    updateTodo(e.target.dataset.id, e.target.classList.contains('done'));
+  }
+};
+
+const updateTodo = (id, completed) => {
+  fetch(`${apiUrl}/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ completed }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+const deleteTodo = (e) => {
+  if (e.target.classList.contains('todo')) {
+    const id = e.target.dataset.id;
+    fetch(`${apiUrl}/${id}`, {
+      method: 'DELETE',
+    })
+      .then((res) => res.json())
+      .then(() => e.target.remove());
+  }
 };
 
 const init = () => {
   document.addEventListener('DOMContentLoaded', getTodos);
-  document.querySelector('#todo-form').addEventListener('submit', createToDo);
+  document.querySelector('#todo-form').addEventListener('submit', createTodo);
+  document
+    .querySelector('#todo-list')
+    .addEventListener('click', toggleCompleted);
+  document.querySelector('#todo-list').addEventListener('dblclick', deleteTodo);
 };
-getTodos();
+
+init();
