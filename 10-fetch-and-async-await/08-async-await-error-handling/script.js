32d7582bd5fa40df38ae6dbfd2c7c72f0790eee1
Nazrul Islam <nazrulislambhat@gmail.com>
2023-06-07 16:50:24 +0530

    fetch async

---
+++ 10-fetch-and-async-await/08-async-await-error-handling/script.js
@@ -0,0 +1,10 @@
+const getUsers = async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+getUsers();
