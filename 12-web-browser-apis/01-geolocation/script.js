3628db0c0e72b61bcf17bab36cb231dbdfa80714
Nazrul Islam <nazrulislambhat@gmail.com>
2023-06-16 11:25:34 +0530

    First Commit

--- 12-web-browser-apis/01-geolocation/script.js
+++ 12-web-browser-apis/01-geolocation/script.js
@@ -1,4 +1,13 @@
-// getCurrentPosition()
+function curSuccess(pos) {
+  console.log(pos);
+}
 
+function curError(err) {
+  console.log(`Error: ${err.code} - ${err.message}`);
+}
+
+const options = {};
+// getCurrentPosition()
+navigator.geolocation.getCurrentPosition(curSuccess, curError, options);
 
 // watchPosition()
