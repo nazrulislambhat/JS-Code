ba1f36ed9eeb601b936533f0736ea00cde636d61
Nazrul Islam <nazrulislambhat@gmail.com>
2023-06-19 15:50:59 +0530

    Commit

---
+++ 13-oop-constructors-prototypes/02-constructor-functions/script.js
@@ -0,0 +1,18 @@
+//construction function
+function Rectangle(name, width, height) {
+  this.name = name;
+  this.width = width;
+  this.height = height;
+}
+
+const rect1 = new Rectangle('Rectangle1', 100, 200);
+
+console.log(rect1)
+//once we use the new keyword
+
+/*
+1:new empty object is created
+2:construction function is call with the arguments we passed in
+3:the 'this' keyword is set to new empty object
+4:the new object is returned from thew construction function
+*/
