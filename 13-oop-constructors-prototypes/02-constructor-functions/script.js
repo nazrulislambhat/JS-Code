9d8abf7193580646e13bd5a25a72a406b6a3e16f
Nazrul Islam <nazrulislambhat@gmail.com>
2023-06-19 15:52:39 +0530

    Commit

--- 13-oop-constructors-prototypes/02-constructor-functions/script.js
+++ 13-oop-constructors-prototypes/02-constructor-functions/script.js
@@ -3,11 +3,16 @@ function Rectangle(name, width, height) {
   this.name = name;
   this.width = width;
   this.height = height;
+  this.area = function () {
+    return this.height * this.width;
+  };
 }
 
 const rect1 = new Rectangle('Rectangle1', 100, 200);
 
-console.log(rect1)
+console.log(rect1);
+
+console.log(rect1.constructor);
 //once we use the new keyword
 
 /*
